470e79b29a9dd948656b80e9a383132a77f67633
Aleh Kapusta <alehkapusta@gmail.com>
2023-11-29 15:05:56 +0100

    add input and button in BookForm and to plug css

---
+++ frontend/src/components/BookForm/BookForm.js
@@ -0,0 +1,29 @@
+import { useState } from "react"
+import './BookForm.css'
+const BookForm = () => {
+  const [title, setTitle] = useState(''),
+    [author, setAuthor] = useState(''),
+    handleSubmit = (e) => {
+      e.preventDefault();
+      if (title && author) {
+        setAuthor('')
+        setTitle('')
+      }
+    }
+
+
+  return (
+    <div className="app-block book-form">
+      <h2>Add a new Book</h2>
+      <form onSubmit={handleSubmit}>
+        <div><label htmlFor="title">Title:</label>
+          <input type="text" id="title" value={title} onChange={(e) => setTitle(e.target.value)} /></div>
+        <div><label htmlFor="author">Author:</label>
+          <input type="text" id="author" value={author} onChange={(e) => setAuthor(e.target.value)} /></div>
+        <button type="submit">Add Book</button>
+      </form>
+    </div>
+  )
+}
+
+export default BookForm
